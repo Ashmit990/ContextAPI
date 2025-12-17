65b4afe4bff91cba53f6dc48e8df13d6f8b411cf
Ashmit <ashmitcristiano07@gmail.com>
2025-12-17 08:16:37 +0545

    initialize theme context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { UserContextProvider } from './components/context/UserContextProvider'
-import { Login } from './components/pages/Login'
-import { Profile } from './components/pages/Profile'
+import { themeProvider } from './components/context/theme'
 
 export const App = () => {
   return (
-    <>
-    <UserContextProvider>
-      <Login/>
-      <Profile/>
-    </UserContextProvider>
-    </>
+    <themeProvider>
+   
+  <div className="flex flex-wrap min-h-screen items-center">
+                  <div className="w-full">
+                      <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                          
+                      </div>
+
+                      <div className="w-full max-w-sm mx-auto">
+                        
+                      </div>
+                  </div>
+              </div>
+
+    </themeProvider>
   )
 }
 
